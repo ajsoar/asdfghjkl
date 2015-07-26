51aad61bf58d1ffd559a1a27ee6ce21e7dd94cba
AJ Dela Cruz <allen-jhane.dela-cru@smash.lpfi.org>
2015-07-26 12:14:02 -0700

    modified speed for game (gradualling speeding ofc)

--- Gameplay.swift
+++ Gameplay.swift
@@ -83,7 +83,7 @@ class Gameplay: CCNode, CCPhysicsCollisionDelegate {
         gamePhysicsNode.position = CGPoint(x: gamePhysicsNode.position.x - scrollSpeed, y: gamePhysicsNode.position.y)
         let velocityY = clampf(Float(character.physicsBody.velocity.y), -Float(CGFloat.max), 300)
         character.physicsBody.velocity = ccp(0, CGFloat(velocityY))
-        scrollSpeed *= 1.0005
+        scrollSpeed *= 1.0007
         
         for ledge in ledges.reverse() {
             let ledgeWorldPosition = gamePhysicsNode.convertToWorldSpace(ledge.position)
